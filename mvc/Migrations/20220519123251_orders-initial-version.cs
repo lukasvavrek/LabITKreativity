0d14b46e12a58449b5c97f2e330a52f3e0739e41
Lukáš Vavrek <lukas.vavrek@visma.com>
2022-05-19 17:06:15 +0200

    Generated migration.

---
+++ mvc/Migrations/20220519123251_orders-initial-version.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkladUcebnic.Migrations
+{
+    public partial class ordersinitialversion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StorageNumber",
+                table: "Book",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderState = table.Column<int>(type: "int", nullable: false),
+                    OrderedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClassIdentifier = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookOrder",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookOrder", x => new { x.BookId, x.OrderId });
+                    table.ForeignKey(
+                        name: "FK_BookOrder_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookOrder_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookOrder_OrderId",
+                table: "BookOrder",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookOrder");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StorageNumber",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+        }
+    }
+}
