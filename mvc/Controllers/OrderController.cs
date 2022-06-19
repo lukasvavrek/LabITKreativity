6b422bed6bfde0ec426df25b81a3b0c0a0ca4fa7
Lukáš Vavrek <lukas.vavrek@visma.com>
2022-06-19 23:54:13 +0200

    Finished Create Order workflow.

--- mvc/Controllers/OrderController.cs
+++ mvc/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SkladUcebnic.Data;
@@ -26,13 +28,55 @@ namespace SkladUcebnic.Controllers
         }
 
         // GET: Orders/Create
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
-            var books = await _dbContext.Book.ToListAsync();
-            var bookOrders = books.ConvertAll<BookOrder>(book => new BookOrder { Book = book, BookId = book.Id });
-            var order = new Order { BookOrders = bookOrders };
+            return View(await GetNewOrderAsync());
+        }
 
-            return View(order);
+        // POST: Orders/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind]Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.BookOrders = order.BookOrders.Where(bookOrder => bookOrder.Quantity > 0).ToList();
+                
+                order.OrderState = OrderState.New;
+                order.OrderedAt = DateTime.Now;
+
+                await _dbContext.AddAsync(order);
+                await _dbContext.SaveChangesAsync();
+                
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(await GetOrderWithAlreadyFilledDataAsync(order));
+        }
+
+        private async Task<Order> GetNewOrderAsync()
+        {
+            var books = await _dbContext.Book.ToListAsync();
+            var bookOrders = books.ConvertAll(book => new BookOrder {Book = book, BookId = book.Id});
+            return new Order {BookOrders = bookOrders};
+        }
+
+        private async Task<Order> GetOrderWithAlreadyFilledDataAsync(Order order)
+        {
+            var newOrder = await GetNewOrderAsync();
+
+            newOrder.ClassIdentifier = order.ClassIdentifier;
+            newOrder.OrderedBy = order.OrderedBy;
+            newOrder.BookOrders.ForEach(bookOrder =>
+            {
+                var existingBookOrder = order.BookOrders.SingleOrDefault(bo => bo.BookId == bookOrder.BookId);
+                if (existingBookOrder != null)
+                {
+                    bookOrder.Quantity = existingBookOrder.Quantity;
+                }
+            });
+            return newOrder;
         }
     }
 }
